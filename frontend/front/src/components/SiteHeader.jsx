0b929435058dd12663ae11fb28feaec8b12ba5f0
Cube219 <max804800@gmail.com>
2022-12-01 10:32:14 +0900

    frontend: Add force refresh button

--- frontend/front/src/components/SiteHeader.jsx
+++ frontend/front/src/components/SiteHeader.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import {Layout, Typography, Button, Row, Col } from 'antd';
+import {Layout, Typography, Button, Row, Col, message } from 'antd';
 import {
   LogoutOutlined,
+  RedoOutlined
 } from '@ant-design/icons'
 import './design/SiteHeader.css';
 import {useNavigate} from "react-router-dom";
+
+import axios from 'axios';
+import Cookies from "universal-cookie";
+
+const cookies = new Cookies();
 
 const { Title } = Typography;
 const { Header} = Layout;
@@ -14,13 +20,33 @@ export function SiteHeader(){
 
   const navigate = useNavigate();
 
+  const forceRefresh = async () => {
+    try {
+      await axios({
+          url: `api/dev/force`,
+          method: "post",
+          headers: {
+              "x-access-token": cookies.get('access_token')
+          },
+      });
+      window.location.reload();
+    }
+    catch (ex) {
+        message.error("Can't refresh state");
+        return;
+    }
+  };
+
   return(
     <Header className="site-layout-background" style={{ padding: 0 }}>
       <Row>
         <Col span={8} offset={8}>
           <Title level={2} className="site-title">Crowler</Title>
         </Col>
-        <Col offset={7}>
+        <Col offset={5}>
+          <Button type="primary" shape='circle' icon={<RedoOutlined />} onClick={()=>forceRefresh()}/>
+        </Col>
+        <Col offset={1}>
           <Button type="primary" shape='circle' icon={<LogoutOutlined/>} danger onClick={()=>navigate("/")}/>
         </Col>
       </Row>
